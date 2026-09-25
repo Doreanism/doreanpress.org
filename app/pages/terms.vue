8947bd9a7c6fd7b887aa26bf25dfed1fa8ba7dff
MasterTemple <dgmastertemple@gmail.com>
2026-09-25 07:41:04 -0700

    Fixed typo

--- app/pages/terms.vue
+++ app/pages/terms.vue
@@ -85,7 +85,7 @@ const updated = 'September 2026'
           Copyright
         </h2>
         <p>
-          All books published by Dorean Press are dedicated to the public domain. You can copy, translate, modify, and distribute all resources, without restriction, and without needing to ask permission. All resource are freely given (Matt 10:8) for the sake of the gospel.
+          All books published by Dorean Press are dedicated to the public domain. You can copy, translate, modify, and distribute all resources, without restriction, and without needing to ask permission. All resources are freely given (Matt 10:8) for the sake of the gospel.
         </p>
         <a href="https://freely.giving/free">
           <img src="https://copy.church/badges/fg_standard_pde.svg">
